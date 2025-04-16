15b9c81411d61cc73df3baf346af3751c1f67c82
Deryx E. Scott <deryxescott@gmail.com>
2025-04-16 18:33:34 -0400

    Update VocabularyCard, VerbCard, and Conjugation components with React Native components

--- src/components/flashcards/VerbCard.tsx
+++ src/components/flashcards/VerbCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Verb from '../../types/Verb';
 import Conjugation from '../ui/Conjugation';
 import Conjugations from '../../types/Conjugations';
